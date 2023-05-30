0e3545e4c363231bb29b0a6648191402b3ce2551
Natalija_L <natalija.liske@gmail.com>
2023-05-30 14:45:21 +0300

    numbers

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+
+Esminiai konceptai programavime:
+
+1) kintamieji - informacijos atsiminimas;
+2) If'ai - info palyginimas
+3) loop'ai (ciklai) - operaciju valdymas N kartu
+4) functions - pasikartojancios logikos aprasymas
+
+*/
