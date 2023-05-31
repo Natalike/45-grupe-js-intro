b38092f1b8c9370cffecd8e5090a59513c8489c8
Natalija_L <natalija.liske@gmail.com>
2023-05-31 14:15:54 +0300

    boolean

---
+++ js/04-boolean.js
@@ -0,0 +1,23 @@
+console.clear();
+
+/*
+BOOLEAN - logine reiksme
+true, falsa
+
+Loginiai operatoriai:
+&& (and) - ir
+|| (or) - arba
+
+*/
+
+const isOld = true;
+console.log(isOld);
+
+const isFunny = falsse;
+console.log(isFunny);
+
+console.log(true && true);
+
+console.clear();
+
+console.log('-----------------------');
