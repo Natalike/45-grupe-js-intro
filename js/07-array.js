6d4489aa8e9295a21d02e3b3ed5229ca96ee0c74
Natalija_L <natalija.liske@gmail.com>
2023-06-02 09:02:54 +0300

    array

--- js/07-array.js
+++ js/07-array.js
@@ -1,34 +1,42 @@
 console.clear();
-
 /*
-ARRAY - sarasas, matrica, masyvas, arejus, listas
+ARRAY - sąrašas, matrica, masyvas, arėjus, listas
+
+[].length - elementu kiekis masyve
 */
 
 const jonas = [10, 2, 8];
-const maryte = [];
-const petras = [];
-const ona = [];
+const maryte = [10, 9, 8, 7];
+const petras = [5, 6];
+const ona = [10, 3, 3, 3, 10, 10, 10];
 
 console.log(jonas);
 
-console.log('------------------------------');
+const abc = ['a', 'b', 'c', 'd', 'e', 'f'];
+console.log(abc);
 
-const students = ['Jonas', 'Maryte', 'Petras', "ona"];
+console.log('-----------');
+
+//                  0         1         2       3       4
+const students = ['Jonas', 'Maryte', 'Petras', 'Ona', 'Martynas'];
 console.log(students);
 console.log(students[0]);
 console.log(students[1]);
 console.log(students[2]);
 console.log(students[3]);
+console.log(students[4]);
+console.log(students[45]);
+console.log(students[414562]);
+console.log(students[-1]);
+console.log(students[-2]);
 
-
-
-console.clear(); 
+console.log('----------------');
 
 function suma(masyvas) {
-    return masyvas[1] + masyvas[2] + masyvas[3];
+    return masyvas[0] + masyvas[1] + masyvas[2];
 }
 
-const al = [1, 2, 3];
+const a1 = [1, 2, 3];
 const s1 = suma(a1);
 
 const a2 = [10, 9, 8];
@@ -41,33 +49,41 @@ console.log(s1, '->', 6);
 console.log(s2, '->', 27);
 console.log(s3, '->', 15);
 
+console.log(students.length);
+console.log(abc.length);
+
+const empty = [];
+console.log(empty, empty.length);
+
+const index = students.length - 1;
+console.log('Paskutinis:', students[index]);
+
 console.clear();
 
-//Jonas. Paskutinis pazymys yra 8.
-//Maryte. Paskutinis pazymys yra 8.
-//Petras. Paskutinis pazymys yra 7.
-//Ona. Paskutinis pazymys yra 5.
+// Jonas: paskutinis pazymys yra 8.
+// Maryte: paskutinis pazymys yra 8.
+// Petras: paskutinis pazymys yra 7.
+// Ona: paskutinis pazymys yra 5.
 
-const stud1 ='Jonas';
+const stud1 = 'Jonas';
 const marks1 = [10, 2, 8, 2, 8, 2, 8];
-
-const stud2 ='Maryte';
-const marks2 = [10, 9, 8,];
-
-const stud3 ='Petras';
+const stud2 = 'Maryte';
+const marks2 = [10, 9, 8];
+const stud3 = 'Petras';
 const marks3 = [5, 6, 7];
-
-const stud4 ='Ona';
+const stud4 = 'Ona';
 const marks4 = [5, 5, 5, 5, 5, 5, 5, 5];
 
-function studentIntro(pazymys) {
-    return 
+function studentIntro(name, marks) {
+    const lastIndex = marks.length - 1;
+    const lastMark = marks[lastIndex];
+    return `${name}: paskutinis pazymys yra ${lastMark}.`;
 }
 
-console.log(studentIntro());
-console.log(studentIntro());
-console.log(studentIntro());
-console.log(studentIntro());
+console.log(studentIntro(stud1, marks1));
+console.log(studentIntro(stud2, marks2));
+console.log(studentIntro(stud3, marks3));
+console.log(studentIntro(stud4, marks4));
 
 
 
