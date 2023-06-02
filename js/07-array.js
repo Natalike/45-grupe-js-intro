fec30308e7be513061e9ed37bcc4abfb73389e09
Natalija_L <natalija.liske@gmail.com>
2023-06-02 09:02:03 +0300

    functions

---
+++ js/07-array.js
@@ -0,0 +1,74 @@
+console.clear();
+
+/*
+ARRAY - sarasas, matrica, masyvas, arejus, listas
+*/
+
+const jonas = [10, 2, 8];
+const maryte = [];
+const petras = [];
+const ona = [];
+
+console.log(jonas);
+
+console.log('------------------------------');
+
+const students = ['Jonas', 'Maryte', 'Petras', "ona"];
+console.log(students);
+console.log(students[0]);
+console.log(students[1]);
+console.log(students[2]);
+console.log(students[3]);
+
+
+
+console.clear(); 
+
+function suma(masyvas) {
+    return masyvas[1] + masyvas[2] + masyvas[3];
+}
+
+const al = [1, 2, 3];
+const s1 = suma(a1);
+
+const a2 = [10, 9, 8];
+const s2 = suma(a2);
+
+const a3 = [5, 5, 5];
+const s3 = suma(a3);
+
+console.log(s1, '->', 6);
+console.log(s2, '->', 27);
+console.log(s3, '->', 15);
+
+console.clear();
+
+//Jonas. Paskutinis pazymys yra 8.
+//Maryte. Paskutinis pazymys yra 8.
+//Petras. Paskutinis pazymys yra 7.
+//Ona. Paskutinis pazymys yra 5.
+
+const stud1 ='Jonas';
+const marks1 = [10, 2, 8, 2, 8, 2, 8];
+
+const stud2 ='Maryte';
+const marks2 = [10, 9, 8,];
+
+const stud3 ='Petras';
+const marks3 = [5, 6, 7];
+
+const stud4 ='Ona';
+const marks4 = [5, 5, 5, 5, 5, 5, 5, 5];
+
+function studentIntro(pazymys) {
+    return 
+}
+
+console.log(studentIntro());
+console.log(studentIntro());
+console.log(studentIntro());
+console.log(studentIntro());
+
+
+
+
